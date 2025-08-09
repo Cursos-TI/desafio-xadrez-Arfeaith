19b19524b6d1c1479e7d68829ef4bbc0d7231f39
Vitor Pereira <vitordspereira00@gmail.com>
2025-08-09 16:40:47 -0300

    Nível Mestre v1.0

--- xadrez.c
+++ xadrez.c
@@ -1,52 +1,65 @@
 #include <stdio.h>
 
-int main() {
-
-    int torre = 1;
-    int bispo = 1;
-    int rainha = 1;
-    int cavalo = 1;
-
-    printf("Movimento da torre:\n");
-
-    for (torre; torre <= 5; torre++)
+//Criando funções recursivas para as peças
+void moverTorre (int casasTorre){
+    if (casasTorre > 0)
     {
         printf("Direita\n");
+        moverTorre(casasTorre - 1);
     }
+    
+}
+
+void moverBispo (int casasBispo){
+    if (casasBispo > 0)
+    {
+        printf("Cima, Direita\n");
+        moverBispo(casasBispo - 1);
+    }
+    
+}
+
+void moverRainha (int casasRainha){
+    if (casasRainha > 0)
+    {
+        printf("Esquerda\n");
+        moverRainha(casasRainha - 1);
+    }
+    
+}
+
+int main() {
+
+    //Criando variável para o caso especial do cavalo
+    int cavalo = 1;
+
+    //Imprimindo as funções recursivas
+    printf("Movimento da torre:\n");
+
+    moverTorre(5);
 
     printf("\nMovimento do bispo:\n");
 
-    do
-    {
-
-        printf("Cima, Direita\n");
-        bispo++;
-
-    } while (bispo <= 5);
+    moverBispo(5);
     
     printf("\nMovimento da rainha:\n");
 
-    while (rainha <= 8)
-    {
-        printf("Esquerda\n");
-        rainha++;
-    }
+    moverRainha(8);
 
-    while (cavalo <= 1)
+    //Criando o loop para o movimento do cavalo
+    while (cavalo > 0)
     {
 
         printf("\nMovimento do cavalo:\n");
 
-        for (int i = 0; i < 2; i++)
+        for (int i = 2; i > 0; i--)
         {
             printf("Cima\n");
         }
     
         printf("Direita\n");
-        cavalo++;
+        cavalo--;
     }
-    
-    
 
     return 0;
 }
